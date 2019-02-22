a9579897655bdf3a95bdf812f0647370282a1144
MelaatiJ <fathimajayah876@gmail.com>
2019-02-22 16:42:14 -0600

    fixed it

--- CwProject/CwApp/views.py
+++ CwProject/CwApp/views.py
@@ -3,17 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 
 #functions that connect to my webpages
-def index(request, name):
-    return render(request, 'icApp/index.html')
+def index(request):
+    return render(request, 'CwApp/index.html')
 
 
-def base(request, name):
-    return render(request, 'icApp/details.html')
+def base(request):
+    return render(request, 'CwApp/base.html')
 
 
 
-def about(request, name):
-    return render(request, 'icApp/details.html')
+def about(request):
+    return render(request, 'CwApp/about.html')
 
 
 
